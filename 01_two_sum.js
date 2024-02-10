32cfe7e89e41a11c3d6de53dd156b778e1084031
Shu <luo.s@outlook.com>
2024-02-09 23:12:07 -0500

    good copy

--- 01_two_sum.js
+++ 01_two_sum.js
@@ -8,4 +8,15 @@ may not use the same element twice.
 You can return the answer in any order.
 */
 
-var twoSum = function (nums, target) {};
+// I cannot use the same element twice, so I need to keep track of the
+// elements I have already used. I can use a hash table.
+var twoSum = function (nums, target) {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let number = target - nums[i];
+    if (map.has(number)) {
+      return [map.get(number), i];
+    }
+    map.set(nums[i], i);
+  }
+};
