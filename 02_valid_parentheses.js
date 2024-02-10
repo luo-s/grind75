46d2bc4c28d75631ac09550605a4e3307a106e08
Shu <luo.s@outlook.com>
2024-02-10 17:40:34 -0500

    good copy

--- 02_valid_parentheses.js
+++ 02_valid_parentheses.js
@@ -11,4 +11,29 @@ Every close bracket has a corresponding open bracket of the same type.
 
 // https://leetcode.com/problems/valid-parentheses/description/
 
-var isValid = function (s) {};
+// data structure: stack
+var isValid = function (s) {
+  // define a stack
+  const stack = [];
+  // define a map for matching parentheses
+  const map = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+  // loop through the string
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]]) {
+      // if the character is an open bracket, push it to the stack
+      stack.push(s[i]);
+    } else {
+      // if the character is a close bracket, pop the last element from the stack
+      const last = stack.pop();
+      if (s[i] !== map[last]) {
+        return false;
+      }
+    }
+  }
+  // if the stack is empty, return true
+  return stack.length === 0;
+};
