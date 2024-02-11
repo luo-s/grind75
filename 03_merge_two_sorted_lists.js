04a9fb689129394e8d3d3e6ea853220a3745f3e1
Shu <luo.s@outlook.com>
2024-02-10 22:49:15 -0500

    good copy

--- 03_merge_two_sorted_lists.js
+++ 03_merge_two_sorted_lists.js
@@ -5,6 +5,50 @@ Merge the two lists into one sorted list. The list should be made by
 splicing together the nodes of the first two lists.
 
 Return the head of the merged linked list.
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ * 
 */
 
-var mergeTwoLists = function (list1, list2) {};
+var mergeTwoLists = function (list1, list2) {
+  let dummy = new ListNode();
+  let current = dummy;
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+  if (list1) {
+    current.next = list1;
+  } else {
+    current.next = list2;
+  }
+  return dummy.next;
+};
+
+// recursive solution
+var mergeTwoLists = function (list1, list2) {
+  if (!list1 || !list2) {
+    return !list1 ? list2 : list1;
+  }
+  if (list1.val < list2.val) {
+    list1.next = mergeTwoLists(list1.next, list2);
+    return list1;
+  } else {
+    list2.next = mergeTwoLists(list1, list2.next);
+    return list2;
+  }
+};
