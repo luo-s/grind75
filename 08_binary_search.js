6f4b556fcabc786812f87aebe678951597c22c32
Shu Luo <luo.s@outlook.com>
2024-02-13 10:21:54 -0500

    wip

--- 08_binary_search.js
+++ 08_binary_search.js
@@ -13,5 +13,16 @@ You must write an algorithm with O(log n) runtime complexity.
 // https://leetcode.com/problems/binary-search/description/
 
 var search = function(nums, target) {
-    
+    let left = 0, right = nums.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] > target) {
+            right = mid - 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
 };
