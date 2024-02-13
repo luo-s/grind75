1bd60bc675f5a36ae3af5ec1f619ffdc396a74c6
Shu <luo.s@outlook.com>
2024-02-12 22:04:23 -0500

    good copy

--- 06_invert_binary_tree.js
+++ 06_invert_binary_tree.js
@@ -3,3 +3,12 @@ Given the root of a binary tree, invert the tree, and return its root.
 */
 
 //https://leetcode.com/problems/invert-binary-tree/description/
+
+var invertTree = function (root) {
+  if (!root) return null;
+  let left = invertTree(root.left);
+  let right = invertTree(root.right);
+  root.left = right;
+  root.right = left;
+  return root;
+};
