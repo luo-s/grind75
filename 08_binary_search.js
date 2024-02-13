4c0b1661b42a9e148b0e5a38a520155536c3ea11
Shu Luo <luo.s@outlook.com>
2024-02-13 10:09:24 -0500

    wip

---
+++ 08_binary_search.js
@@ -0,0 +1,17 @@
+/*
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. 
+If target exists, then return its index. Otherwise, return -1.
+
+1 <= nums.length <= 10^4
+-10^4 < nums[i], target < 10^4
+All the integers in nums are unique.
+nums is sorted in ascending order.
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+
+// https://leetcode.com/problems/binary-search/description/
+
+var search = function(nums, target) {
+    
+};
