0209b1e94f0320741f2e637a566cb02cbcffb4c6
Shu <luo.s@outlook.com>
2024-02-12 22:10:34 -0500

    hash map

--- 07_valid_anagram.js
+++ 07_valid_anagram.js
@@ -5,6 +5,25 @@ and false otherwise.
 An Anagram is a word or phrase formed by rearranging the letters of a 
 different word or phrase, typically using all the original letters exactly 
 once.
+
+s and t consist of lowercase English letters.
 */
 
-var isAnagram = function (s, t) {};
+// https://leetcode.com/problems/valid-anagram/description/
+
+// hash map solution
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+  let sMap = new Map();
+  for (let char of s) {
+    sMap.set(char, sMap.get(char) + 1 || 1);
+  }
+  for (let char of t) {
+    if (!sMap.has(char)) return false;
+    sMap.set(char, sMap.get(char) - 1);
+  }
+  for (let [key, value] of sMap) {
+    if (value !== 0) return false;
+  }
+  return true;
+};
