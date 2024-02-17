8553eef9e0d4a43cedc8864397c66f70aaee5cd1
Shu Luo <luo.s@outlook.com>
2024-02-17 15:48:50 -0500

    wip

---
+++ 15_ransom_note.js
@@ -0,0 +1,11 @@
+/*
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+*/
+
+// https://leetcode.com/problems/ransom-note/description/
+
+var canConstruct = function(ransomNote, magazine) {
+    
+};
