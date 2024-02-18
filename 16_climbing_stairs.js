078d47a6b2747aa93c09e254cbd049ec825a7b12
Shu <luo.s@outlook.com>
2024-02-18 18:32:42 -0500

    recursive solution

--- 16_climbing_stairs.js
+++ 16_climbing_stairs.js
@@ -8,4 +8,7 @@ In how many distinct ways can you climb to the top?
 // https://leetcode.com/problems/climbing-stairs/
 
 // recursive solution
-var climbStairs = function (n) {};
+var climbStairs = function (n) {
+  if (n === 1 || n === 2) return n;
+  return climbStairs(n - 1) + climbStairs(n - 2);
+};
