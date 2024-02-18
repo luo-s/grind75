46df9d2b1f03e11c13b174ba56093c362d9e4de9
Shu Luo <luo.s@outlook.com>
2024-02-17 19:04:29 -0500

    wip

--- 15_ransom_note.js
+++ 15_ransom_note.js
@@ -6,6 +6,19 @@ Each letter in magazine can only be used once in ransomNote.
 
 // https://leetcode.com/problems/ransom-note/description/
 
+// hash table
 var canConstruct = function(ransomNote, magazine) {
-    
+    if (ransomNote.length > magazine.length) return false;
+    let map = new Map();
+    for (let char of ransomNote) {
+        map.set(char, map.get(char) || 0 + 1);
+    }
+    for (let char of magazine) {
+        if (!map.has(char)) return false;
+        else {
+            map.set(char, map.get(char) - 1);
+            if (map.get(char) === 0) map.delete(char);
+        }
+    }
+    return true;
 };
