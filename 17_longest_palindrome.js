c6b5d139b545d166c71ebf4e41583471b22c2793
Shu <luo.s@outlook.com>
2024-02-18 21:26:14 -0500

    wip

---
+++ 17_longest_palindrome.js
@@ -0,0 +1,14 @@
+/*
+Given a string s which consists of lowercase or uppercase letters, return 
+the length of the longest palindrome that can be built with those letters.
+
+Letters are case sensitive, for example, "Aa" is not considered a 
+palindrome here.
+
+1 <= s.length <= 2000
+s consists of lowercase and/or uppercase English letters only.
+*/
+
+// https://leetcode.com/problems/longest-palindrome
+
+var longestPalindrome = function (s) {};
