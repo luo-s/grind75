81df95ca67c14d8114d7a51e78642e5bc7779379
Shu <luo.s@outlook.com>
2024-02-18 22:15:34 -0500

    wip

--- 18_reverse_linked_list.js
+++ 18_reverse_linked_list.js
@@ -1,8 +1,34 @@
 /*
 Given the head of a singly linked list, reverse the list, 
 and return the reversed list.
+
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
 */
 
 // https://leetcode.com/problems/reverse-linked-list/description/
 
-var reverseList = function (head) {};
+// construct another linked list in reverse order
+// create a array to store the values of the linked list
+// time complexity: O(n)
+// space complexity: O(n)
+var reverseList = function (head) {
+  if (!head || !head.next) return head;
+  let arr = [];
+  let curr = head;
+  while (curr) {
+    arr.push(curr.val);
+    curr = curr.next;
+  }
+  let newHead = new ListNode(arr.pop());
+  let ans = newHead;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    let node = new ListNode(arr[i]);
+    newHead.next = node;
+    newHead = newHead.next;
+  }
+  return ans;
+};
