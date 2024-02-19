afe41f61360b0eead5709ac79bd5f859498d1d82
Shu <luo.s@outlook.com>
2024-02-18 21:34:04 -0500

    hash table

--- 17_longest_palindrome.js
+++ 17_longest_palindrome.js
@@ -11,4 +11,24 @@ s consists of lowercase and/or uppercase English letters only.
 
 // https://leetcode.com/problems/longest-palindrome
 
-var longestPalindrome = function (s) {};
+// hash map solution
+var longestPalindrome = function (s) {
+  let max = 0;
+  // construct a dictionary of the characters in the string
+  let map = new Map();
+  for (let char of s) {
+    map.set(char, (map.get(char) || 0) + 1);
+  }
+  // iterate through the dictionary and add the number of pairs of characters
+  for (let [key, value] of map.entries()) {
+    max += Math.floor(value / 2) * 2;
+  }
+  // if there are any characters left, add one to the max (the middle of the palindrome)
+  for (let [key, value] of map.entries()) {
+    if (value % 2 === 1) {
+      max++;
+      break;
+    }
+  }
+  return max;
+};
