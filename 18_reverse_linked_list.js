278c4e0e6ef09ba6508ea4d619747960672972d4
Shu <luo.s@outlook.com>
2024-02-18 21:36:59 -0500

    wip

---
+++ 18_reverse_linked_list.js
@@ -0,0 +1,8 @@
+/*
+Given the head of a singly linked list, reverse the list, 
+and return the reversed list.
+*/
+
+// https://leetcode.com/problems/reverse-linked-list/description/
+
+var reverseList = function (head) {};
