4ef0658baaab621ea41ac76c37d680374101fdd6
Shu <luo.s@outlook.com>
2024-02-20 09:22:21 -0500

    wip

---
+++ 22_middle_of_the_linked_list.js
@@ -0,0 +1,13 @@
+/*
+Given the head of a singly linked list, return the middle node of the 
+linked list.
+
+If there are two middle nodes, return the second middle node.
+
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
+*/
+
+// https://leetcode.com/problems/middle-of-the-linked-list/description/
+
+var middleNode = function (head) {};
