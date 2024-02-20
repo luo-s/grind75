db2e5d766b38e4c125c64fd97a0e85b09dd2bc8f
Shu <luo.s@outlook.com>
2024-02-20 07:27:46 -0500

    wip

---
+++ 20_add_binary.js
@@ -0,0 +1,11 @@
+/*
+Given two binary strings a and b, return their sum as a binary string.
+
+1 <= a.length, b.length <= 104
+a and b consist only of '0' or '1' characters.
+Each string does not contain leading zeros except for the zero itself.
+*/
+
+// https://leetcode.com/problems/add-binary/
+
+var addBinary = function (a, b) {};
