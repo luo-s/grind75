5dd26e4a2598a4c0b9ac3fb86fa4a0609063cd83
Shu <luo.s@outlook.com>
2024-02-20 07:43:29 -0500

    brute force

--- 20_add_binary.js
+++ 20_add_binary.js
@@ -1,11 +1,31 @@
 /*
 Given two binary strings a and b, return their sum as a binary string.
 
-1 <= a.length, b.length <= 104
+1 <= a.length, b.length <= 10^4
 a and b consist only of '0' or '1' characters.
 Each string does not contain leading zeros except for the zero itself.
 */
 
 // https://leetcode.com/problems/add-binary/
 
-var addBinary = function (a, b) {};
+// brute force
+// time complexity: O(n^2) (unshift operation is O(n))
+// space complexity: O(n)
+var addBinary = function (a, b) {
+  let arrA = a.split(""),
+    arrB = b.split(""),
+    ans = [];
+  let carry = 0;
+  while (arrA.length || arrB.length) {
+    let sum = (arrA.pop() || 0) * 1 + (arrB.pop() || 0) * 1 + carry;
+    if (sum > 1) {
+      sum %= 2;
+      carry = 1;
+    } else {
+      carry = 0;
+    }
+    ans.unshift(sum);
+  }
+  if (carry) ans.unshift(1);
+  return ans.join("");
+};
