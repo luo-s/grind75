190a24e5192015ad0a60cf95745b46d0b7d5ae48
Shu <luo.s@outlook.com>
2024-02-19 21:04:36 -0500

    hash table

--- 19_majority_element.js
+++ 19_majority_element.js
@@ -3,6 +3,21 @@ Given an array nums of size n, return the majority element.
 
 The majority element is the element that appears more than ⌊n / 2⌋ times. 
 You may assume that the majority element always exists in the array.
+
+n == nums.length
+1 <= n <= 5 * 10^4
+-10^9 <= nums[i] <= 10^9
 */
 
-var majorityElement = function (nums) {};
+// hash table
+// time complexity O(n)
+// space complexity O(n)
+var majorityElement = function (nums) {
+  let map = new Map();
+  for (let num of nums) {
+    map.set(num, (map.get(num) || 0) + 1);
+  }
+  for (let [key, value] of map.entries()) {
+    if (value > nums.length / 2) return key;
+  }
+};
