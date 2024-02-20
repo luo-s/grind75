322c498542af36925919f82d7cec19508201596b
Shu <luo.s@outlook.com>
2024-02-20 08:38:32 -0500

    brute force

--- 21_diameter_of_binary_tree.js
+++ 21_diameter_of_binary_tree.js
@@ -10,4 +10,25 @@ edges between them.
 
 // https://leetcode.com/problems/diameter-of-binary-tree/description/
 
-var diameterOfBinaryTree = function (root) {};
+// brute force -- huge call stack (nested recursive calls)
+var diameterOfBinaryTree = function (root) {
+  // define a helper function to calculate the depth of a tree
+  var depth = function (node) {
+    if (!node) return 0;
+    let left = depth(node.left),
+      right = depth(node.right);
+    return Math.max(left, right) + 1;
+  };
+  let maxDiameter = 0;
+  // define a helper function to calculate the diameter of a tree
+  var dfs = function (node) {
+    if (!node) return maxDiameter;
+    let left = depth(node.left),
+      right = depth(node.right);
+    maxDiameter = Math.max(maxDiameter, left + right);
+    dfs(node.left);
+    dfs(node.right);
+    return maxDiameter;
+  };
+  return dfs(root);
+};
