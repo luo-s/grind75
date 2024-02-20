e702faddfa969f623269e5b1c8ee41c43e3dc788
Shu <luo.s@outlook.com>
2024-02-20 09:20:23 -0500

    good copy

--- 21_diameter_of_binary_tree.js
+++ 21_diameter_of_binary_tree.js
@@ -10,11 +10,13 @@ edges between them.
 
 // https://leetcode.com/problems/diameter-of-binary-tree/description/
 
+// diameter(node) = depth(node.left) + depth(node.right)
+// then depth first seatrch to find the max diameter
 // brute force -- huge call stack (nested recursive calls)
 var diameterOfBinaryTree = function (root) {
   // define a helper function to calculate the depth of a tree
   var depth = function (node) {
-    if (!node) return 0;
+    if (!node) return 0; // base case
     let left = depth(node.left),
       right = depth(node.right);
     return Math.max(left, right) + 1;
@@ -22,7 +24,7 @@ var diameterOfBinaryTree = function (root) {
   let maxDiameter = 0;
   // define a helper function to calculate the diameter of a tree
   var dfs = function (node) {
-    if (!node) return maxDiameter;
+    if (!node) return; // base case
     let left = depth(node.left),
       right = depth(node.right);
     maxDiameter = Math.max(maxDiameter, left + right);
@@ -31,4 +33,20 @@ var diameterOfBinaryTree = function (root) {
     return maxDiameter;
   };
   return dfs(root);
+};
+
+// optimized solution
+var diameterOfBinaryTree = function (root) {
+  let maxDiameter = 0;
+  var depth = function (node) {
+    if (!node) return 0; // base case
+    let left = depth(node.left),
+      right = depth(node.right);
+    // update the max diameter
+    maxDiameter = Math.max(maxDiameter, left + right);
+    // return the depth of the tree
+    return Math.max(left, right) + 1;
+  };
+  depth(root);
+  return maxDiameter;
 };
