cb31615e0257802955821b8dd9c9925d1d74bc4e
Shu <luo.s@outlook.com>
2024-02-20 09:27:15 -0500

    two pointers

--- 22_middle_of_the_linked_list.js
+++ 22_middle_of_the_linked_list.js
@@ -10,4 +10,27 @@ The number of nodes in the list is in the range [1, 100].
 
 // https://leetcode.com/problems/middle-of-the-linked-list/description/
 
-var middleNode = function (head) {};
+// array solution
+// time complexity: O(n)
+// space complexity: O(n)
+var middleNode = function (head) {
+  let arr = [];
+  while (head) {
+    arr.push(head);
+    head = head.next;
+  }
+  return arr[Math.floor(arr.length / 2)];
+};
+
+// two pointers
+// time complexity: O(n)
+// space complexity: O(1)
+var middleNode = function (head) {
+  let slow = head,
+    fast = head;
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+  return slow;
+};
