c3a9653d03b274cf80ab422b1d8bfa343597626b
Shu <luo.s@outlook.com>
2024-02-21 08:24:37 -0500

    brute force

--- 25_maximum_subarray.js
+++ 25_maximum_subarray.js
@@ -4,4 +4,19 @@ and return its sum.
 
 // https://leetcode.com/problems/maximum-subarray/description/
 
-var maxSubArray = function (nums) {};
+// brute force
+// time complexity: O(n^3)
+// space complexity: O(1)
+var maxSubArray = function (nums) {
+  let max = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i; j < nums.length; j++) {
+      let sum = 0;
+      for (let k = i; k <= j; k++) {
+        sum += nums[k];
+      }
+      max = Math.max(max, sum);
+    }
+  }
+  return max;
+};
