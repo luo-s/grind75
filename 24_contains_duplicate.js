a275cde3164a71a68218a6644924f63586920d53
Shu <luo.s@outlook.com>
2024-02-21 08:13:06 -0500

    wip

---
+++ 24_contains_duplicate.js
@@ -0,0 +1,11 @@
+/*
+Given an integer array nums, return true if any value appears at least 
+twice in the array, and return false if every element is distinct.
+
+1 <= nums.length <= 10^5
+-10^9 <= nums[i] <= 10^9
+*/
+
+// https://leetcode.com/problems/contains-duplicate/description/
+
+var containsDuplicate = function (nums) {};
