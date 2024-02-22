6f515e7e0ce3716c616ecfbbf3f07948bb14d4ba
Shu <luo.s@outlook.com>
2024-02-22 08:09:50 -0500

    sorting

--- 28_k_closest_points_to_origin.js
+++ 28_k_closest_points_to_origin.js
@@ -18,19 +18,30 @@ The answer is guaranteed to be unique (except for the order that it is in).
 // time complexity: O(nlogn)
 // space complexity: O(n)
 var kClosest = function (points, k) {
-  let distMap = new Map();
-  let distArr = [];
-  let res = [];
+  let res = [],
+    distMap = new Map(),
+    distArr = [];
+  // map the distance of each point to the origin
   for (let [x, y] of points) {
-    const distance = Math.sqrt(x * x + y * y);
+    const distance = x * x + y * y;
     distArr.push(distance);
     distMap.set([x, y], distance);
   }
+  // sort the distance array, the kth closest distance is distArr[k - 1]
   distArr.sort((a, b) => a - b);
+  // filter out the points that have distance <= distArr[k - 1]
   for (let [point, distance] of distMap.entries()) {
     if (distance <= distArr[k - 1]) {
       res.push(point);
     }
   }
   return res;
+};
+
+// sorting, no need to use hash table
+// time complexity: O(nlogn)
+// space complexity: O(1)
+var kClosest = function (points, k) {
+  points.sort((a, b) => a[0] ** 2 + a[1] ** 2 - (b[0] ** 2 + b[1] ** 2));
+  return points.slice(0, k);
 };
