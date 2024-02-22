7d84c78f18e8101139ba660b6a6423a027e7736f
Shu <luo.s@outlook.com>
2024-02-22 07:35:37 -0500

    hash table + sorting

--- 28_k_closest_points_to_origin.js
+++ 28_k_closest_points_to_origin.js
@@ -14,4 +14,23 @@ The answer is guaranteed to be unique (except for the order that it is in).
 
 // https://leetcode.com/problems/k-closest-points-to-origin/description/
 
-var kClosest = function (points, k) {};
+// hash table + sorting
+// time complexity: O(nlogn)
+// space complexity: O(n)
+var kClosest = function (points, k) {
+  let distMap = new Map();
+  let distArr = [];
+  let res = [];
+  for (let [x, y] of points) {
+    const distance = Math.sqrt(x * x + y * y);
+    distArr.push(distance);
+    distMap.set([x, y], distance);
+  }
+  distArr.sort((a, b) => a - b);
+  for (let [point, distance] of distMap.entries()) {
+    if (distance <= distArr[k - 1]) {
+      res.push(point);
+    }
+  }
+  return res;
+};
