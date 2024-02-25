3afaf3de7a41ca8e413243270051c6876a5ca4f8
Shu <luo.s@outlook.com>
2024-02-25 12:24:03 -0500

    good copy

--- 31_binary_tree_level_order_traversal.js
+++ 31_binary_tree_level_order_traversal.js
@@ -10,12 +10,12 @@ var levelOrder = function (root) {
   // base case
   if (!root) return [];
   let result = [];
-  var bfs = function (node, level, result) {
+  var bfs = function (node, level, array) {
     if (node === null) return;
-    if (result[level] === undefined) result[level] = [];
-    result[level].push(node.val);
-    bfs(node.left, level + 1, result);
-    bfs(node.right, level + 1, result);
+    if (array[level] === undefined) array[level] = [];
+    array[level].push(node.val);
+    bfs(node.left, level + 1, array);
+    bfs(node.right, level + 1, array);
   };
   bfs(root, 0, result);
   return result;
@@ -26,12 +26,12 @@ var levelOrder = function (root) {
   // base case
   if (!root) return [];
   let result = [];
-  var dfs = function (node, level, result) {
+  var dfs = function (node, level, array) {
     if (node === null) return;
-    if (result[level] === undefined) result[level] = [];
-    result[level].push(node.val);
-    dfs(node.left, level + 1, result);
-    dfs(node.right, level + 1, result);
+    if (array[level] === undefined) array[level] = [];
+    array[level].push(node.val);
+    dfs(node.left, level + 1, array);
+    dfs(node.right, level + 1, array);
   };
   dfs(root, 0, result);
   return result;
