6c04f39f6b0dc0be750d050472e1c315fb831e6f
Shu <luo.s@outlook.com>
2024-02-25 12:57:45 -0500

    wip

---
+++ 34_course_schedule.js
@@ -0,0 +1,20 @@
+/*
+There are a total of numCourses courses you have to take, labeled from 
+0 to numCourses - 1. You are given an array prerequisites where 
+prerequisites[i] = [ai, bi] indicates that you must take course bi first 
+if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to 
+first take course 1.
+Return true if you can finish all courses. Otherwise, return false.
+
+1 <= numCourses <= 2000
+0 <= prerequisites.length <= 5000
+prerequisites[i].length == 2
+0 <= ai, bi < numCourses
+All the pairs prerequisites[i] are unique.
+*/
+
+// https://leetcode.com/problems/course-schedule/
+
+var canFinish = function (numCourses, prerequisites) {};
