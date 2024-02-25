f9946b548dd534aa097744c7febeabfefcfbc418
Shu <luo.s@outlook.com>
2024-02-24 23:44:32 -0500

    brute force

--- 29_longest_substring_without_repeating_characters.js
+++ 29_longest_substring_without_repeating_characters.js
@@ -2,3 +2,31 @@
 Given a string s, find the length of the longest substring without 
 repeating characters.
 */
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+// brute force -- time limit exceeded
+// time complexity: O(n^3)
+// space complexity: O(n)
+var lengthOfLongestSubstring = function (s) {
+  var allUnique = function (s, start, end) {
+    let set = new Set();
+    let str = s.substring(start, end);
+    for (let char of str) {
+      if (set.has(char)) {
+        return false;
+      }
+      set.add(char);
+    }
+    return true;
+  };
+  let max = 0;
+  for (let i = 0; i < s.length; i++) {
+    for (let j = i + 1; j <= s.length; j++) {
+      if (allUnique(s, i, j)) {
+        max = Math.max(max, j - i);
+      }
+    }
+  }
+  return max;
+};
