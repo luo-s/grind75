08aa384b0104823f77a64c34b8a95da73697cd60
Shu <luo.s@outlook.com>
2024-02-25 12:18:38 -0500

    bfs

--- 31_binary_tree_level_order_traversal.js
+++ 31_binary_tree_level_order_traversal.js
@@ -5,4 +5,18 @@ nodes' values. (i.e., from left to right, level by level).
 
 // https://leetcode.com/problems/binary-tree-level-order-traversal/description/
 
-var levelOrder = function (root) {};
+// breadth first search
+var levelOrder = function (root) {
+  // base case
+  if (!root) return [];
+  let result = [];
+  var bfs = function (node, level, result) {
+    if (node === null) return;
+    if (result[level] === undefined) result[level] = [];
+    result[level].push(node.val);
+    if (node.left) bfs(node.left, level + 1, result);
+    if (node.right) bfs(node.right, level + 1, result);
+  };
+  bfs(root, 0, result);
+  return result;
+};
