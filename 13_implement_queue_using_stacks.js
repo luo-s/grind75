a8ba8b445aa3c01013fafbf027f1b5aecb47d857
Shu <luo.s@outlook.com>
2024-02-26 07:36:00 -0500

    wip

---
+++ 13_implement_queue_using_stacks.js
@@ -0,0 +1,37 @@
+/*
+Implement a first in first out (FIFO) queue using only two stacks. 
+The implemented queue should support all the functions of a normal queue 
+(push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+*/
+
+// https://leetcode.com/problems/implement-queue-using-stacks/
+
+var MyQueue = function () {};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {};
