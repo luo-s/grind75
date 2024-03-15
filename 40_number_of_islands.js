2648a43618fe29f32a8bc2250a948929e71a20be
Shu <luo.s@outlook.com>
2024-03-15 00:46:19 -0400

    dfs

--- 40_number_of_islands.js
+++ 40_number_of_islands.js
@@ -18,21 +18,22 @@ var numIslands = function (grid) {
     n = grid[0].length;
   var dfs = function (grid, i, j) {
     if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] === "0") {
-      return 0;
+      return;
     } else {
       grid[i][j] = "0";
       dfs(grid, i + 1, j);
       dfs(grid, i - 1, j);
       dfs(grid, i, j + 1);
       dfs(grid, i, j - 1);
-      return 1;
+      return;
     }
   };
   let ans = 0;
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (grid[i][j] === "1") {
-        ans += dfs(grid, i, j);
+        ans += 1;
+        dfs(grid, i, j);
       }
     }
   }
