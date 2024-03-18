999ad122e23438f2fe1b8b0f0fb66ddef1091b13
Shu <luo.s@outlook.com>
2024-03-18 13:13:34 -0400

    good copy

--- 42_search_in_rotated_sorted_array.js
+++ 42_search_in_rotated_sorted_array.js
@@ -11,8 +11,57 @@ Given the array nums after the possible rotation and an integer target,
 return the index of target if it is in nums, or -1 if it is not in nums.
 
 You must write an algorithm with O(log n) runtime complexity.
+
+All values of nums are unique.
+nums is an ascending array that is possibly rotated.
 */
 
 // https://leetcode.com/problems/search-in-rotated-sorted-array/description/
 
-var search = function (nums, target) {};
+/*
+rotated ascending array: split the array into two ascending parts
+*/
+
+var search = function (nums, target) {
+  if (nums.length === 1) return nums[0] === target ? 0 : -1;
+  let left = 0,
+    right = nums.length - 1;
+  if (target >= nums[0]) {
+    // target is in the left part
+    while (left <= right) {
+      // stop condition: left > right
+      let mid = Math.floor((left + right) / 2);
+      if (nums[mid] === target) return mid;
+      if (nums[mid] >= nums[0]) {
+        // mid is in the left part
+        if (nums[mid] > target) {
+          right = mid - 1;
+        } else if (nums[mid] < target) {
+          left = mid + 1;
+        }
+      } else if (nums[mid] < nums[0]) {
+        // mid is in the right part
+        right = mid - 1;
+      }
+    }
+  } else {
+    // target is in the right part
+    while (left <= right) {
+      // stop condition: left > right
+      let mid = Math.floor((left + right) / 2);
+      if (nums[mid] === target) return mid;
+      if (nums[mid] >= nums[0]) {
+        // mid is in the left part
+        left = mid + 1;
+      } else if (nums[mid] < nums[0]) {
+        // mid is in the right part
+        if (nums[mid] > target) {
+          right = mid - 1;
+        } else if (nums[mid] < target) {
+          left = mid + 1;
+        }
+      }
+    }
+  }
+  return -1;
+};
