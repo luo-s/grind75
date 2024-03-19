26dd37a4124989d782dd29fd7cf77b1ba7d3fd1f
Shu <luo.s@outlook.com>
2024-03-19 10:15:02 -0400

    good copy

--- 45_merge_intervals.js
+++ 45_merge_intervals.js
@@ -6,4 +6,23 @@ intervals that cover all the intervals in the input.
 
 // https://leetcode.com/problems/merge-intervals/description/
 
-var merge = function (intervals) {};
+/* sort & merge
+sort the intervals by start time ASC
+merge: extend the latest interval end if overlap
+*/
+var merge = function (intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  let result = [];
+  for (let interval of intervals) {
+    if (result.length === 0) result.push(interval);
+    if (interval[0] > result[result.length - 1][1]) {
+      result.push(interval);
+    } else {
+      result[result.length - 1][1] = Math.max(
+        result[result.length - 1][1],
+        interval[1]
+      );
+    }
+  }
+  return result;
+};
