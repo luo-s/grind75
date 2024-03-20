219ae9c552b67b63cbf0443ce6405fa6e4969ac8
Shu <luo.s@outlook.com>
2024-03-19 23:34:22 -0400

    good copy

--- 49_sort_colors.js
+++ 49_sort_colors.js
@@ -15,7 +15,31 @@ nums[i] is either 0, 1, or 2.
 
 // https://leetcode.com/problems/sort-colors/description/
 
+// three pointers
+// time complexity: O(n)
+// space complexity: O(1)
+var sortColors = function (nums) {
+  let low = 0;
+  let mid = 0;
+  let high = nums.length - 1;
+  while (mid <= high) {
+    if (nums[mid] === 0) {
+      [nums[low], nums[mid]] = [nums[mid], nums[low]];
+      low++;
+      mid++;
+    } else if (nums[mid] === 2) {
+      [nums[mid], nums[high]] = [nums[high], nums[mid]];
+      high--;
+    } else {
+      mid++;
+    }
+  }
+  return nums;
+};
+
 // two pointers, sort twice
+// time complexity: O(n)
+// space complexity: O(1)
 var sortColors = function (nums) {
   // move all 0's to the left
   let left = 0,
@@ -35,6 +59,45 @@ var sortColors = function (nums) {
       [nums[left], nums[right]] = [nums[right], nums[left]];
       right--;
     } else left++;
+  }
+  return nums;
+};
+
+// two pointers, one sort
+// time complexity: O(n^2)
+// space complexity: O(1)
+var sortColors = function (nums) {
+  let left = 0,
+    right = nums.length - 1;
+  while (left < right) {
+    if (nums[left] === 0) {
+      left++;
+    } else if (nums[right] === 2) {
+      right--;
+    } else if (nums[left] === 2) {
+      [nums[left], nums[right]] = [nums[right], nums[left]];
+      right--;
+    } else if (nums[right] === 0) {
+      [nums[left], nums[right]] = [nums[right], nums[left]];
+      left++;
+    } else {
+      // when nums[left] === 1 and nums[right] === 1
+      if (right - 1 === left) {
+        break;
+      } else {
+        let nextZero = nums.indexOf(0, left);
+        let nextTwo = nums.lastIndexOf(2, right);
+        if (nextZero !== -1) {
+          [nums[left], nums[nextZero]] = [nums[nextZero], nums[left]];
+          left++;
+        }
+        if (nextTwo !== -1) {
+          [nums[right], nums[nextTwo]] = [nums[nextTwo], nums[right]];
+          right--;
+        }
+        if (nextZero === -1 && nextTwo === -1) break;
+      }
+    }
   }
   return nums;
 };
