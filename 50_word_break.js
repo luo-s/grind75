445a808c515003003f4f4cff007843b544105fe1
Shu <luo.s@outlook.com>
2024-03-19 23:47:55 -0400

    dfs with memoization

--- 50_word_break.js
+++ 50_word_break.js
@@ -18,3 +18,23 @@ var wordBreak = function (s, wordDict) {
   }
   return false;
 };
+
+// dfs with memoization
+var wordBreak = function (s, wordDict) {
+  let memo = new Map();
+  var dfs = function (s) {
+    if (memo.has(s)) return memo.get(s);
+    for (let word of wordDict) {
+      if (s.startsWith(word)) {
+        let newS = s.slice(word.length);
+        if (newS.length === 0 || dfs(newS)) {
+          memo.set(s, true);
+          return true;
+        }
+      }
+    }
+    memo.set(s, false);
+    return false;
+  };
+  return dfs(s);
+};
