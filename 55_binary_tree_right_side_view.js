4b0acee01eb7330d95d9e1a577281f3faae5f4cb
Shu <luo.s@outlook.com>
2024-03-21 21:18:05 -0400

    good copy

--- 55_binary_tree_right_side_view.js
+++ 55_binary_tree_right_side_view.js
@@ -7,4 +7,22 @@ to bottom.
 // https://leetcode.com/problems/binary-tree-right-side-view/description/
 
 // find the rightmost node at each level
-var rightSideView = function (root) {};
+// level order traversal
+var rightSideView = function (root) {
+  let res = [];
+  var levelOrder = function (node) {
+    if (!node) return;
+    let queue = [node];
+    while (queue.length) {
+      let len = queue.length; // number of nodes at this level
+      for (let i = 0; i < len; i++) {
+        let curr = queue.shift();
+        if (i === len - 1) res.push(curr.val);
+        if (curr.left) queue.push(curr.left);
+        if (curr.right) queue.push(curr.right);
+      }
+    }
+  };
+  levelOrder(root);
+  return res;
+};
