229d65afde394c5e1959f65a57ad8c9ac77a941b
Shu <luo.s@outlook.com>
2024-03-24 18:38:13 -0400

    good copy

--- 59_container_with_most_water.js
+++ 59_container_with_most_water.js
@@ -7,11 +7,32 @@ Find two lines that together with the x-axis form a container, such that
 the container contains the most water.
 
 Return the maximum amount of water a container can store.
-
 Notice that you may not slant the container.
 
+n == height.length
+2 <= n <= 105
+0 <= height[i] <= 104
 */
 
 // https://leetcode.com/problems/container-with-most-water/description/
 
-var maxArea = function (height) {};
+/* two pointers
+- start with the widest container
+- always move the shorter line inwards
+*/
+var maxArea = function (height) {
+  let max = 0,
+    left = 0,
+    right = height.length - 1;
+  while (left < right) {
+    let width = right - left;
+    if (height[left] > height[right]) {
+      max = Math.max(max, width * height[right]);
+      right--;
+    } else {
+      max = Math.max(max, width * height[left]);
+      left++;
+    }
+  }
+  return max;
+};
