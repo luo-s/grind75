51f94675be4ba5f41e20e0e6e3e199064a51c808
Shu <40323042+luo-s@users.noreply.github.com>
2024-09-16 21:02:15 -0400

    good copy

---
+++ 44_permutations.py
@@ -0,0 +1,30 @@
+# Given an array nums of distinct integers, return all the possible permutations. 
+# You can return the answer in any order.
+
+# All the integers of nums are unique.
+
+# https://leetcode.com/problems/permutations/
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res, path, l = [], [], len(nums)
+        used = [False] * l
+        nums.sort()
+        def backtracking(nums):
+            # base case
+            if len(path) == l:
+                res.append(path[:])
+                return
+            # recursive case
+            for i in range(l):
+                # if this element has been used
+                if used[i]:
+                    continue
+                path.append(nums[i])
+                used[i] = True
+                backtracking(nums)
+                path.pop()
+                used[i] = False
+                
+        backtracking(nums)
+        return res
